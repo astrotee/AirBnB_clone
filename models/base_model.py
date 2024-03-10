e7b1072bc813cab978b4934a9d1a3b31fc1ec4c0
amin ahmed <aminahmed24@gmail.com>
2024-03-10 04:18:22 +0200

    create models package and chmod +x

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,20 @@ import datetime
 class BaseModel:
     """base model"""
 
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         """initialize an instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            d = kwargs
+            d.pop("__class__")
+            if "created_at" in d:
+                d["created_at"] = datetime.datetime.fromisoformat(d.created_at)
+            if "updated_at" in d:
+                d["updated_at"] = datetime.datetime.fromisoformat(d.updated_at)
+            self.__dict__.update(d)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self) -> str:
         """a string representation of instance"""
